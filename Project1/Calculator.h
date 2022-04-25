f7604700b50424ce3c921da8e54875b8fd37b007
IamXDD1 <oliver920324@gmail.com>
2022-04-25 14:32:58 +0800

    update Calculator.h

--- Project1/Calculator.h
+++ Project1/Calculator.h
@@ -13,19 +13,18 @@
 using namespace std;
 
 class Calculator {
-	map<string, Number> exist_var;
 	int weight(char op);
 public:
+	static map<string, Number> exist_var;
 	void RUN();
-	string Input(bool& equal);
+	Number Input(bool& equal);
 	void judgeFormat(string infix); //判斷名字 若有變數則直接替換
 
 	Number calculate(string posfix);
 	bool isVariable(string str);
 	auto is_Var_exist(string name);
-	void power_convert(vector<string>& input, int index);
 
 	string InfixtoPosfix(string infix);
-	void Output(string ans);
+	void Output(Number ans);
 	void test();
 };
