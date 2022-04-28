a1c7779a9639ea7b26faa4736d5e8027c1622863
IamXDD1 <oliver920324@gmail.com>
2022-04-28 20:35:01 +0800

    GUI 4/28

---
+++ Project1_GUI/Project1_GUI/Calculator.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <regex>
+#include <stack>
+#include <map>
+#include "Number.h"
+using std::cin;
+using std::cout;
+using std::getline;
+using std::endl;
+using std::map;
+using std::string;
+using std::vector;
+using std::regex;
+using std::stack;
+
+class Calculator {
+	int weight(char op);
+public:
+	static map<string, Number> exist_var;
+	void RUN();
+	Number Input(bool& equal);
+	void judgeFormat(string infix); //判斷名字 若有變數則直接替換
+
+	Number calculate(string posfix);
+	bool isVariable(string str);
+	auto is_Var_exist(string name);
+
+	string InfixtoPosfix(string infix);
+	string Output(Number ans);
+	void test();
+};
---
+++ Project1_GUI/Project1_GUI/MyForm.cpp
@@ -0,0 +1,33 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+
+void main(array<String^>^ args)
+
+{
+	Calculator Cal;
+
+    Application::EnableVisualStyles();
+
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    Project1GUI::MyForm form;
+
+    Application::Run(% form);
+
+	/*
+	try {
+		Cal.test();
+		//Cal.RUN()
+	}
+	catch (char const* error) {
+		cout << error << '\n';
+	}
+	
+	std::cout << "Stop running!";
+	*/
+
+}
+    
