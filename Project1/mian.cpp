a31189c829434baf34b99d81c8562ce9db1656f6
IamXDD1 <oliver920324@gmail.com>
2022-05-11 17:17:37 +0800

    5/11 API declare with char*

--- Project1/mian.cpp
+++ Project1/mian.cpp
@@ -4,41 +4,14 @@
 
 int main() {
 
-	
-	integer x("123456789");
-	Decimal y("3.1415926");
-
-	cout << x << endl;
-	cout << y << endl;
-
-	cin >> x; // 輸入 123456789
-	cin >> y; // 輸入 3.1415926
-
-	
-	cout << x + y << endl;
-	cout << x - y << endl;
-	cout << x * y << endl;
-	cout << x / y << endl;
-	
-
-	x = "123 * 8 + 456";
-	y = "-1.0 / 3 - 45 / 13.0";
-
-	cout << x << endl;
-	cout << y << endl;
+	integer x = "123 * 8 + 456";
+	Decimal y = "-1.0 / 3 - 45 / 13.0";
 
 	vector<Number*> nums;
 	nums.push_back(&x);
 	nums.push_back(&y);
 	for (const auto& num : nums)
 		cout << *num << endl;
-
-	/*
-	cout << x + y << endl;
-	cout << x - y << endl;
-	cout << x * y << endl;
-	cout << x / y << endl;
-	*/
 	
 	
 	return 0;
